8e652936e5a8b0584882b9d3eb670c74528663e4
Alexey Murz Korepov <MurzNN@gmail.com>
2023-05-10 05:31:14 +0000

    Increase to 10 items per query

--- frontend/src/ContentComponent.js
+++ frontend/src/ContentComponent.js
@@ -1,31 +1,28 @@
 import React from 'react';
 const ContentComponent = ({ content }) => {
 	return (
-			<div className="wrapper">
-				<div className="container">
-
-					{
-
-						content.map((item, index) => {
-							return (
-								<div key={index}>
-									<h2 >Title: {item[0].title[0].value}</h2>
-									<div>{item[0].body[0].summary.substring(0, 600)}</div>
-									<hr/>
-									<h2 >Title: {item[1].title[0].value}</h2>
-									<div>{item[1].body[0].summary.substring(0, 600)}</div>
-									<hr/>
-									<h2 >Title: {item[2].title[0].value}</h2>
-									<div>{item[2].body[0].summary.substring(0, 600)}</div>
-									<hr/>
-								</div>
-
-							);
-						})
-					}
-
-				</div>
+		<div className="wrapper">
+			<div className="container">
+				{
+					content.map((item, index) => (
+						<div key={index}>
+							<h2>Bundle {index + 1}:</h2>
+							{
+								item.map((data, dataIndex) => {
+									return (
+										<div key={index + '_' + dataIndex}>
+											<h3 >Node {data.nid[0]?.value}: {data.title[0]?.value}</h3>
+											<div>{data.body[0]?.summary.substring(0, 600)}</div>
+											<hr />
+										</div>
+									);
+								})
+							}
+						</div>
+					))
+				}
 			</div>
+		</div>
 
 	);
 };
